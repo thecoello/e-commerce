c400a77b8f0a8d0886aa4cd69acbc214d36ff19b
Manuel Guerra Coello <thecoello@gmail.com>
2021-02-08 11:19:44 +0100

    Creación de vistas para user y exportación de router para implementar en clase Router

---
+++ src/es/e-commerce/views/userView.ts
@@ -0,0 +1,31 @@
+import express = require('express');
+const router =  express.Router();
+import UserControllers = require('../Controller Implements/UserImplements');
+
+
+/**************************
+Create user
+**************************/
+router.post('/user/register',UserControllers.Create);
+
+/**************************
+Get user
+**************************
+router.get('/user',UserControllers.Get)
+
+/**************************
+Update user
+ **************************
+router.put('/user/update',UserControllers.Update);
+
+/**************************
+Delete user
+ **************************
+router.delete('/user/delete',UserControllers.Delete);
+
+/**************************
+ Logging user
+ **************************
+router.post('/login',UserControllers.Login)*/
+
+export = router;
