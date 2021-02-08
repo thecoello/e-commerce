4089b5bd8c397c5719b142b9c8e1dac6258db551
Manuel Guerra Coello <thecoello@gmail.com>
2021-02-08 08:55:06 +0100

    Modificación de nombre de la colección

--- src/es/e-commerce/models/products.ts
+++ src/es/e-commerce/models/products.ts
@@ -22,5 +22,5 @@ const ProductSchema = new mongoose.Schema({
     }
 });
 
-const Product = mongoose.model('Porduct', ProductSchema);
+const Product = mongoose.model('Products', ProductSchema);
 export = Product;
