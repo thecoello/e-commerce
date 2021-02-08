4b0277df0bf9b0c635bf808dd52f278cdd9c04b5
Manuel Guerra Coello <thecoello@gmail.com>
2021-02-08 17:38:58 +0100

    Implementación de rutas de productos

--- src/es/e-commerce/views/Views Router/RouterImpl.ts
+++ src/es/e-commerce/views/Views Router/RouterImpl.ts
@@ -1,9 +1,16 @@
 import  { app } from '../../app';
 import { Router } from './Router'
 import UserRouter = require('../userView');
+import ProductsView = require('../productsView');
 
 /***************
 Router for Users
 ****************/
 const RouterUser = new Router(app,UserRouter,'/').router();
+
+/***************
+Router for Products
+****************/
+const RouterPrducts = new Router(app,ProductsView,'/').router();
+
 
