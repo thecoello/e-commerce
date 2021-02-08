d67f7db1c52f2f99878242a495fdffc3489588db
Manuel Guerra Coello <thecoello@gmail.com>
2021-02-08 12:07:24 +0100

    Implementación de actualización de usuario

--- src/es/e-commerce/controllers/userController/UserCRUD.ts
+++ src/es/e-commerce/controllers/userController/UserCRUD.ts
@@ -9,15 +9,15 @@ export class UserCrud extends ControllerCrud {
         super(new DBDataCrud())
     }
 
-    async Create(param:any,req:any,res:any,msg:string) {
+    async Create(param: any, req: any, res: any, msg: string) {
         try {
-            const user = await this.CRUD.find(param,req,res,{ email: req.body.email })
-         
-            if(user[0]){
+            const user = await this.CRUD.find(param, req, res, { email: req.body.email })
+
+            if (user[0]) {
                 res.end('this user exist');
             }
 
-            const createUser = await this.CRUD.create(param,req,res,req.body)
+            const createUser = await this.CRUD.create(param, req, res, req.body)
             return await res.send({ message: msg, object: createUser });
 
         } catch (error) {
@@ -25,27 +25,47 @@ export class UserCrud extends ControllerCrud {
         }
     }
 
-    async Read(param:any,req:any,res:any,msg:string) {
+    async Read(param: any, req: any, res: any, msg: string) {
         try {
-            const user = await this.CRUD.find(param,req,res,{ email: req.body.email })
-            
-            if(!user[0]){
+            const user = await this.CRUD.find(param, req, res, { email: req.body.email })
+
+            if (!user[0]) {
                 res.end('this user exist');
             }
 
-            return await res.send({ message: msg, object: user[0] });
-         
+            const token = await TokenAction.ValToken(user[0]._id, req, res)
+
+            if (token === true) {
+                return await res.send({ message: msg, object: user[0] });
+            }
 
         } catch (error) {
-            return ErrorCatch.errorReturn(error, res, 'There was a problem creating the user')
+            return ErrorCatch.errorReturn(error, res, 'There was a problem getting the user')
         }
     }
 
-    async Update(param:any,req:any,res:any,msg:string) {
+    async Update(param: any, req: any, res: any, msg: string) {
+        try {
+            const user = await this.CRUD.find(param, req, res, { email: req.body.email })
 
+            if (!user[0]) {
+                return res.status(404).end('This user does not Exist');
+            }
+
+            const token = await TokenAction.ValToken(user[0]._id, req, res)
+
+            if (token === true) {
+                const userUpdate = await this.CRUD.update(param, req.body, res, { email: user[0].email })
+                userUpdate.save()
+                return await res.send({ message: msg, object: userUpdate });
+            }
+
+        } catch (error) {
+            return ErrorCatch.errorReturn(error, res, 'There was a problem updating the user')
+        }
     }
 
-    async Delete(param:any,req:any,res:any,msg:string) {
+    async Delete(param: any, req: any, res: any, msg: string) {
     }
 
 }
