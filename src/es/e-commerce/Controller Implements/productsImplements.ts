27d191771c361fae6b801085b3f1bf6ba1a9bff8
Manuel Guerra Coello <thecoello@gmail.com>
2021-02-08 17:41:04 +0100

    implementación de las instancias para el CRUD de productos e implementarlo en las vistas de producto

---
+++ src/es/e-commerce/Controller Implements/productsImplements.ts
@@ -0,0 +1,32 @@
+import { ProductsCrud } from "../controllers/productsController/productsCRUD";
+import Product = require("../models/products");
+
+class ProductsControllers  {
+    static async Create(req, res) {
+        const Post = await new ProductsCrud()
+        await Post.Create(Product, req, res, "Product Registered");
+    }
+    static async Get(req, res) {
+        const Post = await new ProductsCrud()
+        await Post.Read(Product, req, res, "Product Founded");
+    }
+    static async GetPublic(req, res) {
+        const Post = await new ProductsCrud()
+        await Post.ReadPublic(Product, req, res, "Products Founded");
+    }
+    static async GetPublicByCategory(req, res) {
+        const Post = await new ProductsCrud()
+        await Post.ReadPublicbyCategory(Product, req, res, "Products Founded");
+    }
+    static async Update(req, res) {
+        const Post = await new ProductsCrud()
+        await Post.Update(Product, req, res, "Product Updated");
+    }
+    static async Delete(req, res) {
+        const Post = await new ProductsCrud()
+        await Post.Delete(Product, req, res, "Product Deleted");
+    }
+
+}
+
+export = ProductsControllers;
