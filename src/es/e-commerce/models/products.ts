5868b634da1644a9d1d79d384664f07a28b91355
Manuel Guerra Coello <thecoello@gmail.com>
2021-02-08 08:54:24 +0100

    Creación de modelo de productos

---
+++ src/es/e-commerce/models/products.ts
@@ -0,0 +1,26 @@
+import mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "the name for the product it's required"]
+    },
+    description: {
+        type: String,
+    },
+    price: {
+        type: String,
+        required: [true, "The price for the product it's required"]
+    },
+    category: {
+        type: String,
+        required: [true, "The category for the product it's required"],
+    },
+    seller: {
+        type: String,
+        required: [true, "The seller for the product it's required"],
+    }
+});
+
+const Product = mongoose.model('Porduct', ProductSchema);
+export = Product;
