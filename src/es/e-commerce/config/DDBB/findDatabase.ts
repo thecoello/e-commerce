a80ce991ceb7647c74e55432f297185376f171e6
Manuel Guerra Coello <thecoello@gmail.com>
2021-02-16 12:25:31 +0100

    Eliminación de linea mal creada

--- src/es/e-commerce/config/DDBB/findDatabase.ts
+++ src/es/e-commerce/config/DDBB/findDatabase.ts
@@ -4,10 +4,17 @@ const MongoClient = require('mongodb').MongoClient;
 
 export class findDatabase {
     static async find(dataBaseName: String, collectionName: String, toFind: any) {
-
         const db = await MongoClient.connect(urlDatabase)
-
         return await db.db(dataBaseName).collection(collectionName).find(toFind).toArray()
-        
+    }
+
+    static async findAndUpdate(dataBaseName: String, collectionName: String, toFind: any, toUpdate: any) {
+        const db = await MongoClient.connect(urlDatabase)
+        return await db.db(dataBaseName).collection(collectionName).findOneAndUpdate(toFind,{$set: toUpdate},{new:true})
+    }
+
+    static async findAndDelete(dataBaseName: String, collectionName: String, toDelete: any) {
+        const db = await MongoClient.connect(urlDatabase)
+        return await db.db(dataBaseName).collection(collectionName).findOneAndDelete(toDelete)
     }
 }
